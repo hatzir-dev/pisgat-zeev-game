c5257f06a44d58dc6d8f3e74cc03faa105e9ca21
This User <g0527111319@gmail.com>
2026-07-12 19:50:39 +0300

    Remove car fire on race crashes

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service worker: makes the game installable and playable offline.
 // The whole game is one big index.html, so we cache the app shell and serve it
 // cache-first. Leaderboard requests (workers.dev) always go to the network.
-const CACHE = 'pz-merkaz-v6';
+const CACHE = 'pz-merkaz-v7';
 const SHELL = [
   './',
   './index.html',
